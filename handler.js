82cdc009a45c6704889ca1fdfb23e3f49b5bfb33
Assaf Gannon <assaf.gannon@gmai.com>
2018-03-17 14:47:45 +0200

    updates

--- handler.js
+++ handler.js
@@ -1,9 +1,66 @@
-const { encode, decode } = require('./shortener');
-const { getNewIndex } = require('./get-new-index');
-const { save } = require('./save-to-db');
-const { load } = require('./load-from-db');
+const jwt = require('jsonwebtoken');
+
+const { encode } = require('./lib/shortener');
+const { getNewIndex } = require('./lib/get-new-index');
+const { save } = require('./lib/save-to-db');
+const { load } = require('./lib/load-from-db');
 
 const DOMAIN = 'tiny.tikal.io';
+
+// Set in `enviroment` of serverless.yml
+const AUTH0_CLIENT_ID = process.env.AUTH0_CLIENT_ID;
+const AUTH0_CLIENT_PUBLIC_KEY = process.env.AUTH0_CLIENT_PUBLIC_KEY
+
+// Policy helper function
+const generatePolicy = (principalId, effect, resource) => {
+    const authResponse = {};
+    authResponse.principalId = principalId;
+    if (effect && resource) {
+        const policyDocument = {};
+        policyDocument.Version = '2012-10-17';
+        policyDocument.Statement = [];
+        const statementOne = {};
+        statementOne.Action = 'execute-api:Invoke';
+        statementOne.Effect = effect;
+        statementOne.Resource = resource;
+        policyDocument.Statement[0] = statementOne;
+        authResponse.policyDocument = policyDocument;
+    }
+    return authResponse;
+};
+
+// Reusable Authorizer function, set on `authorizer` field in serverless.yml
+module.exports.auth = (event, context, callback) => {
+    console.log('event', event);
+    if (!event.authorizationToken) {
+        return callback('Unauthorized');
+    }
+
+    const tokenParts = event.authorizationToken.split(' ');
+    const tokenValue = tokenParts[1];
+
+    if (!(tokenParts[0].toLowerCase() === 'bearer' && tokenValue)) {
+        // no auth token!
+        return callback('Unauthorized');
+    }
+    const options = {
+        audience: AUTH0_CLIENT_ID,
+    };
+    try {
+        jwt.verify(tokenValue, AUTH0_CLIENT_PUBLIC_KEY, options, (verifyError, decoded) => {
+            if (verifyError) {
+                console.log('verifyError', verifyError);
+                // 401 Unauthorized
+                console.log(`Token invalid. ${verifyError}`);
+                return callback('Unauthorized');
+            }
+            return callback(null, generatePolicy(decoded.sub, 'Allow', event.methodArn));
+        });
+    } catch (err) {
+        console.log('catch error. Invalid token', err);
+        return callback('Unauthorized');
+    }
+};
 
 module.exports.getLink = (event, context, callback) => {
     if (!event.body) {
@@ -25,6 +82,12 @@ module.exports.getLink = (event, context, callback) => {
         .then(shortUrl => {
             const response = {
                 statusCode: 201,
+                headers: {
+                    /* Required for CORS support to work */
+                    "Access-Control-Allow-Origin": "*",
+                    /* Required for cookies, authorization headers with HTTPS */
+                    "Access-Control-Allow-Credentials": true
+                },
                 body: JSON.stringify({
                     shortUrl: `${DOMAIN}/${shortUrl}`,
                 }),
@@ -45,8 +108,13 @@ module.exports.getUrl = (event, context, callback) => {
             const response = {
                 statusCode: 301,
                 headers: {
-                    Location : url,
+                    Location: url,
+                    /* Required for CORS support to work */
+                    "Access-Control-Allow-Origin": "*",
+                    /* Required for cookies, authorization headers with HTTPS */
+                    "Access-Control-Allow-Credentials": true
                 },
+
                 body: null,
             };
             callback(null, response);
